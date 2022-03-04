7c79aaa50ad70e99027b4f71e8d63039d1df84ae
Harshal Jain <76771864+virajj014@users.noreply.github.com>
2022-03-05 03:09:53 +0530

    Add files via upload

--- js/randomcolor.js
+++ js/randomcolor.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 function randomtheme() {
   const myrandomcolor = [
     "#0AD6A0","#FFDE59","#5CE1E6","#FF9656","#7ED957","#00C2CB","#8C52FF","#FF5757","#FF914D"
@@ -19,24 +19,4 @@ function randomtheme() {
 
 
 }
-=======
-function randomtheme() {
-  const myrandomcolor = [
-    "#0AD6A0","#FFDE59","#5CE1E6","#FF9656","#7ED957","#00C2CB","#8C52FF","#FF5757","#FF914D"
-  ];
-  let root = document.documentElement;
-  root.style.setProperty("--color-1", myrandomcolor[Math.floor(Math.random() * myrandomcolor.length)]);
-  root.style.setProperty("--card-color1", myrandomcolor[Math.floor(Math.random() * myrandomcolor.length)]);
-  root.style.setProperty("--card-color2", myrandomcolor[Math.floor(Math.random() * myrandomcolor.length)]);
-  root.style.setProperty("--card-color3", myrandomcolor[Math.floor(Math.random() * myrandomcolor.length)]);
-  root.style.setProperty("--card-color4", myrandomcolor[Math.floor(Math.random() * myrandomcolor.length)]);
-  root.style.setProperty("--card-color5", myrandomcolor[Math.floor(Math.random() * myrandomcolor.length)]);
-  root.style.setProperty("--card-color6", myrandomcolor[Math.floor(Math.random() * myrandomcolor.length)]);
-  root.style.setProperty("--card-color7", myrandomcolor[Math.floor(Math.random() * myrandomcolor.length)]);
-  root.style.setProperty("--card-color8", myrandomcolor[Math.floor(Math.random() * myrandomcolor.length)]);
-  root.style.setProperty("--card-color9", myrandomcolor[Math.floor(Math.random() * myrandomcolor.length)]);
-  root.style.setProperty("--card-color10", myrandomcolor[Math.floor(Math.random() * myrandomcolor.length)]);
-
-}
->>>>>>> 1b5c797 (added the cards)
 window.onload = randomtheme();
